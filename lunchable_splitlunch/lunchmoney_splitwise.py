92d0272f0ea260d474e0f7c37396854a8ffd2e9a
David Vieira <david.vieira@uptempo.io>
2025-11-21 15:57:36 -0300

    .

--- lunchable_splitlunch/lunchmoney_splitwise.py
+++ lunchable_splitlunch/lunchmoney_splitwise.py
@@ -811,7 +811,7 @@ class SplitLunch(splitwise.Splitwise):
                 ),
             )
             reimbursement_object = split_object.model_copy()
-            reimbursement_object.amount = abs(new_transaction.financial_impact) * -1
+            reimbursement_object.amount = abs(new_transaction.financial_impact)
             reimbursement_object.category_id = self.reimbursement_category.id
             logger.debug(
                 f"Transaction split by Splitwise: {transaction.amount} -> "
